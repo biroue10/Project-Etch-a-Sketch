e45303e26a1e71138f3b409cca1f0ebfbb2d4e99
BIROUE ISAAC <biroueisaac@gmail.com>
2023-06-27 17:05:19 +0100

    Adding favicon to the website

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
-const container = document.querySelector('.container')
-for(let i=0;i<=15;i++){
-    const div = document.createElement('div')
-    div.classList.add('row')
-    container.appendChild(div)
+const container = document.querySelector(".container");
+for (let i = 0; i <= 15; i++) {
+  const div = document.createElement("div");
+  div.classList.add("row");
+  container.appendChild(div);
 }
-const row = document.querySelectorAll('.row')
-row.forEach(function(element){
-    for(let i=0;i<=15;i++){
-        const div2 = document.createElement('div')
-        div2.classList.add('item')
-        element.appendChild(div2)
+const row = document.querySelectorAll(".row");
+row.forEach(function (element) {
+  for (let i = 0; i <= 15; i++) {
+    const div2 = document.createElement("div");
+    div2.classList.add("item");
+    element.appendChild(div2);
+  }
+});
+
+const item = document.querySelectorAll(".item");
+item.forEach(function (element) {
+  element.addEventListener("mousemove", (element) => {
+    element.target.classList.add("background");
+  });
+});
+
+const effacer = document.querySelectorAll("button");
+for (const el of effacer) {
+  el.addEventListener("click", (element) => {
+    if (element.target.textContent === "Clear/Reset") {
+      const couleur = document.querySelectorAll(".background");
+      for (const el of couleur) {
+        el.classList.toggle("background");
+      }
     }
-})
-
-const item = document.querySelectorAll('.item')
-item.forEach(function(element){
-    element.addEventListener('mousemove', (element)=>{
-        element.target.classList.add('background')
-    })
-})
-
-const effacer = document.querySelectorAll('button')
-for(const el of effacer){
-    el.addEventListener('click', (element)=>{
-        if(element.target.textContent==='Clear/Reset'){
-            const couleur = document.querySelectorAll('.background')
-            for(const el of couleur){
-                el.classList.toggle('background')
-            }
-        }
-    })
+  });
 }
-
